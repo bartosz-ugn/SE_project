499cf6722fecbc450169c3674ba9a564f2342a6f
bartosz-ugn <71716212+bartosz-ugn@users.noreply.github.com>
2022-10-06 17:59:31 +0200

    encryption done

--- androidAPP/login/login.php
+++ androidAPP/login/login.php
@@ -4,11 +4,15 @@ if(isset($_POST['email']) && isset($_POST['password'])){
     // Include the necessary files
     require_once "conn.php";
     require_once "validate.php";
+    require_once "encryption.php";
     // Call validate, pass form data as parameter and store the returned value
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
+    //Encryption of specified information to find in database
+    $encryptedEmail = openssl_encrypt($email, $ciphering, $encryption_key, $options, $encryption_iv);
+    $encryptedPassword = openssl_encrypt($password, $ciphering, $encryption_key, $options, $encryption_iv);
     // Create the SQL query string
-    $sql = "SELECT * FROM user where UNAME='".md5($email)."' AND PASSWORD='".md5($password)."';";
+    $sql = "SELECT * FROM user where UNAME='".$encryptedEmail."' AND PASSWORD='".$encryptedPassword."';";
     // Execute the query
     $result = $conn->query($sql);
     // If number of rows returned is greater than 0 (that is, if the record is found), we'll print "success", otherwise "failure"
